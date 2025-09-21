fda708c1a8adbc94cee129619ecce7fc21bd35b4
Sachin Jha <sachin03.nic@gmail.com>
2025-09-21 14:04:36 +0530

    email credentials updated

--- config/emailConfig.js
+++ config/emailConfig.js
@@ -3,10 +3,10 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   service: 'gmail', // or use your preferred service
   auth: {
-    user: 'sachinjhaboss2003@gmail.com',    
-    pass: 'emayroajpvtdvejf'   
-    // user : 'foulathbcm@gmail.com',
-    // pass : 'njhngcjonhnxfubj'
+    // user: 'sachinjhaboss2003@gmail.com',    
+    // pass: 'emayroajpvtdvejf'   
+    user : 'foulathbcm@gmail.com',
+    pass : 'njhngcjonhnxfubj'
   }
 });
 
